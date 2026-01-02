a84989b1b9b85436db1813916ad93f01a14aad99
xinxiaoxiaomeng <38664225+xinxiaoxiaomeng@users.noreply.github.com>
2026-01-02 19:16:28 +0800

    Clarify return statement with a comment
    Added comment to clarify the purpose of return statement.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -7,5 +6,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    return a.exec();
+    return a.exec();    // 开启时间循环
 }
